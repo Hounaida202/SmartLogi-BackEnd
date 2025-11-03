9bec0d2b741ff5bb7790fcb9eafb1e5427822716
Hounaida202 <etrichih@gmail.com>
2025-11-03 20:51:43 +0100

    la recupereation des proprités de la table client zone depuis la base de données

--- demo/src/main/java/com/example/demo/entity/Zone.java
+++ demo/src/main/java/com/example/demo/entity/Zone.java
@@ -1,4 +1,44 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="zone")
 public class Zone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "nom", length = 100)
+    private String nom;
+
+    @Size(max = 20)
+    @Column(name = "code_postal", length = 20)
+    private String codePostal;
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
