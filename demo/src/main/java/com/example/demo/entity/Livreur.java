4227bdac9b7eae9b584dc9ce7763cb58d3937755
Hounaida202 <etrichih@gmail.com>
2025-11-03 20:53:02 +0100

    la recupereation des proprités de la table client livreur la base de données

--- demo/src/main/java/com/example/demo/entity/Livreur.java
+++ demo/src/main/java/com/example/demo/entity/Livreur.java
@@ -1,4 +1,80 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="livreur")
 public class Livreur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "nom", length = 100)
+    private String nom;
+
+    @Size(max = 100)
+    @Column(name = "prenom", length = 100)
+    private String prenom;
+
+    @Size(max = 20)
+    @Column(name = "telephone", length = 20)
+    private String telephone;
+
+    @Size(max = 50)
+    @Column(name = "vehicule", length = 50)
+    private String vehicule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "zone_assignee")
+    private Zone zoneAssignee;
+
+    public Zone getZoneAssignee() {
+        return zoneAssignee;
+    }
+
+    public void setZoneAssignee(Zone zoneAssignee) {
+        this.zoneAssignee = zoneAssignee;
+    }
+
+    public String getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(String vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
