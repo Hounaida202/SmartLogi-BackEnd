f5487363e1d539930add75d457c31fc10c2433b5
Hounaida202 <etrichih@gmail.com>
2025-11-03 20:51:04 +0100

    la recupereation des proprités de la table client destinataire depuis la base de données

--- demo/src/main/java/com/example/demo/entity/Destinataire.java
+++ demo/src/main/java/com/example/demo/entity/Destinataire.java
@@ -1,4 +1,80 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="destinataire")
 public class Destinataire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @Column(name = "nom", length = 100)
+    private String nom;
+
+    @Size(max = 100)
+    @Column(name = "prenom", length = 100)
+    private String prenom;
+
+    @Size(max = 150)
+    @Column(name = "email", length = 150)
+    private String email;
+
+    @Size(max = 20)
+    @Column(name = "telephone", length = 20)
+    private String telephone;
+
+    @Size(max = 255)
+    @Column(name = "adresse")
+    private String adresse;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
