7d0dd6e01058e4683699d34357bd5a08137800b8
Hounaida202 <etrichih@gmail.com>
2025-11-03 20:50:10 +0100

    la recupereation des proprités de la table client produit  depuis la base de données

--- demo/src/main/java/com/example/demo/entity/Colis.java
+++ demo/src/main/java/com/example/demo/entity/Colis.java
@@ -1,4 +1,8 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="colis")
 public class Colis {
 }
--- demo/src/main/java/com/example/demo/entity/Produit.java
+++ demo/src/main/java/com/example/demo/entity/Produit.java
@@ -1,4 +1,73 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="produit")
 public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "nom", nullable = false, length = 100)
+    private String nom;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "categorie", nullable = false, length = 50)
+    private String categorie;
+
+    @NotNull
+    @Column(name = "poids", nullable = false, precision = 10, scale = 2)
+    private BigDecimal poids;
+
+    @NotNull
+    @Column(name = "prix", nullable = false, precision = 10, scale = 2)
+    private BigDecimal prix;
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+    public BigDecimal getPoids() {
+        return poids;
+    }
+
+    public void setPoids(BigDecimal poids) {
+        this.poids = poids;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
