50f095784f893c955e4e04e65e6a5d0f24c52124
Hounaida202 <etrichih@gmail.com>
2025-11-05 21:22:45 +0100

    la creation de la classe mapper entre le dto zone  et l entity

---
+++ demo/src/main/java/com/example/demo/dto/ZoneDTO.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+public class ZoneDTO {
+
+
+    private String nom;
+
+
+    private String codePostal;
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+}
---
+++ demo/src/main/java/com/example/demo/mapper/ZoneMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.DestinataireDTO;
+import com.example.demo.dto.ZoneDTO;
+import com.example.demo.entity.Destinataire;
+import com.example.demo.entity.Zone;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper(componentModel = "spring")
+
+public interface ZoneMapper {
+    ZoneMapper INSTANCE = Mappers.getMapper(ZoneMapper.class);
+
+
+    //    ~~convertir le entite en dto :)
+    ZoneDTO toDTO(Zone zone);
+
+    //    ~~~~convertir le dto en entité :)
+    Zone toEntity(ZoneDTO zoneDTO);
+
+}
