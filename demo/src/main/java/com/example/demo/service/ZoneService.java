64bd3a034671f5996f40b5a79a61cfdda75ec931
Hounaida202 <etrichih@gmail.com>
2025-11-06 01:31:43 +0100

    creation d une zone

--- demo/src/main/java/com/example/demo/service/ZoneService.java
+++ demo/src/main/java/com/example/demo/service/ZoneService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.DestinataireDTO;
 import com.example.demo.dto.ZoneDTO;
 import com.example.demo.entity.Destinataire;
 import com.example.demo.entity.Zone;
+import com.example.demo.mapper.DestinataireMapper;
 import com.example.demo.mapper.ZoneMapper;
 import com.example.demo.repository.DestinataireRepository;
 import com.example.demo.repository.ZoneRepository;
@@ -11,5 +13,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ZoneService {
+    @Autowired
+    private ZoneRepository zoneRepository;
 
+    @Autowired
+    private ZoneMapper zoneMapper;
+
+    public ZoneDTO saveZone(ZoneDTO zoneDTO) {
+        Zone zone = zoneMapper.toEntity(zoneDTO);
+        zoneRepository.save(zone);
+        return zoneMapper.toDTO(zone);
+    }
 }
